03e393c76fa2332654d889423ad61bdf429d218e
manikiranp <manikiranp3@gmail.com>
2020-12-10 21:52:51 +0530

    Added name validation in the form using eventlistner

--- js/salaryinc.js
+++ js/salaryinc.js
@@ -4,3 +4,12 @@ output.textContent=salary.value;
 salary.oninput = function() {
     output.textContent=salary.value;
 }
+const nameinp = document.querySelector('#name');
+const nameError = document.querySelector('.name-error');
+nameinp.oninput = function() {
+    let nameRegex = RegExp(/^[A-Z][a-z]{2,}$/);
+    if(nameRegex.test(nameinp.value))
+    nameError.textContent="";
+    else
+    nameError.textContent="Name format is invalid";
+}; 
