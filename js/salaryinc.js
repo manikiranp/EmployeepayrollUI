8b0c3a97573dc6db714098d310341db98b85c68d
manikiranp <manikiranp3@gmail.com>
2020-12-13 14:27:53 +0530

    Getter and setters are added for all attributes

--- js/salaryinc.js
+++ js/salaryinc.js
@@ -13,3 +13,26 @@ nameinp.oninput = function() {
     else
     nameError.textContent="Name format is invalid";
 }; 
+
+function datefun() {
+    var now = new Date();
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    return `${y}${m < 10 ? '0' : ''}${m}${d < 10 ? '0' : ''}${d}`;
+};
+
+// const inpd = document.getElementById('day');
+// const inpm = document.getElementById('month');
+//const inpy = document.getElementById('Year');
+const inpy = document.querySelector('#Year');
+//var inpdate = inpy.toString()+inpm.toString()+inpd.toString();
+const dateError = document.querySelector('.year-error');
+inpy.addEventListener('option',function() {
+    // let today = datefun();
+    let sam = 2000;
+    if(inpy.value > sam) 
+    dateError.textContent="";
+    else
+    dateError.textContent="Future year not allowed"; 
+});
